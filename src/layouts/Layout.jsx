4f89fb5fa050399bf9a74d9c9df701f5803789ae
Pipul Dolai <Pipul.Dolai@lft.com>
2024-11-21 17:18:05 +0530

    my commit

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Headers from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Sidebar from "../components/sidebar";
+import Sidebar from "./Sidebar/Sidebar";
 
 const Layout = ({ children }) => {
   return (
